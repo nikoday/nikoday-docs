c26a4ec1c018465354d830f2a9d4f7098a9e6470
Leandro Kersting de Freitas <leandro@frekele.org>
2020-12-11 04:02:59 -0300

    Create emotions.js

---
+++ json/emotions.js
@@ -0,0 +1,23 @@
+[
+    {
+        "id": "5fd31957b6e0b8426188674b",
+        "createdDateTime": "2020-12-11T07:01:43.741Z",
+        "lastModifiedDateTime": "2020-12-11T07:01:43.741Z",
+        "name": "HAPPY",
+        "icon": "mdi mdi-emoticon-happy"
+    },
+    {
+        "id": "5fd318f8b6e0b84261886749",
+        "createdDateTime": "2020-12-11T07:00:08.744Z",
+        "lastModifiedDateTime": "2020-12-11T07:00:08.744Z",
+        "name": "NEUTRAL",
+        "icon": "mdi mdi-emoticon-neutral"
+    },
+    {
+        "id": "5fd3190fb6e0b8426188674a",
+        "createdDateTime": "2020-12-11T07:00:31.268Z",
+        "lastModifiedDateTime": "2020-12-11T07:00:31.268Z",
+        "name": "SAD",
+        "icon": "mdi mdi-emoticon-sad"
+    }
+]
